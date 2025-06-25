ec4cedb738bef4e6e3a35642e38141344012a2b0
Jose Hernandez <josealfredo.066@gmail.com>
2025-06-25 13:06:05 -0400

    add Property and User models; implement GET route for properties with database connection

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,6 @@ import Hero from '@/components/Hero';
 import HomeProperties from '@/components/HomeProperties';
 import InfoBoxes from '@/components/InfoBoxes';
 import React from 'react';
-import connectDB from '@/config/database';
 
 const HomePage = () => {
   return (
