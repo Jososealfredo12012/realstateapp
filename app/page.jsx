93c89e89f1b7d419d9c8d2497b3adb06a9cc8d35
Jose Hernandez <josealfredo.066@gmail.com>
2025-06-25 12:39:27 -0400

    add database connection setup and import property data; update package.json and package-lock.json for MongoDB and Mongoose dependencies

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,17 @@
-import Hero from '@/components/Hero'
-import HomeProperties from '@/components/HomeProperties'
-import InfoBoxes from '@/components/InfoBoxes'
-import React from 'react'
-
+import Hero from '@/components/Hero';
+import HomeProperties from '@/components/HomeProperties';
+import InfoBoxes from '@/components/InfoBoxes';
+import React from 'react';
+import connectDB from '@/config/database';
 
 const HomePage = () => {
   return (
     <>
-    <Hero></Hero>
-    <InfoBoxes></InfoBoxes>
-    <HomeProperties></HomeProperties>
+      <Hero></Hero>
+      <InfoBoxes></InfoBoxes>
+      <HomeProperties></HomeProperties>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
