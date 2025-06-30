c37c73d5dbdac9dc63920d6ad62612457c31dda1
Jose Hernandez <josealfredo.066@gmail.com>
2025-06-30 15:14:44 -0400

    feat: update Navbar component; change branding to DuoHome and enhance profile menu interactions

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,8 +11,8 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Navbar = () => {
   const { data: session } = useSession();
-  const profileImage = session?.user?.image
-  const profileEmail = session?.user?.email
+  const profileImage = session?.user?.image;
+  const profileEmail = session?.user?.email;
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
   const [providers, setProviders] = useState(false);
@@ -27,7 +27,6 @@ const Navbar = () => {
 
     setAuthProviders();
   }, []);
-
 
   return (
     <nav className='bg-blue-700 border-b border-blue-500'>
@@ -66,7 +65,7 @@ const Navbar = () => {
               <Image className='h-10 w-auto' src={logo} alt='PropertyPulse' />
 
               <span className='hidden md:block text-white text-2xl font-bold ml-2'>
-                PropertyPulse
+                DuoHome
               </span>
             </Link>
             {/* <!-- Desktop Menu Hidden below md screens --> */}
@@ -181,7 +180,10 @@ const Navbar = () => {
                       className='block px-4 py-2 text-sm text-gray-700'
                       role='menuitem'
                       tabIndex='-1'
-                      id='user-menu-item-0'>
+                      id='user-menu-item-0'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}>
                       Your Profile
                     </Link>
                     <Link
@@ -189,11 +191,18 @@ const Navbar = () => {
                       className='block px-4 py-2 text-sm text-gray-700'
                       role='menuitem'
                       tabIndex='-1'
-                      id='user-menu-item-2'>
+                      id='user-menu-item-2'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}>
                       Saved Properties
                     </Link>
                     <button
-                      className='block px-4 py-2 text-sm text-gray-700'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
+                      className='cursor-pointer block px-4 py-2 text-sm text-gray-700'
                       role='menuitem'
                       tabIndex='-1'
                       id='user-menu-item-2'>
