40f1a599d9089608fee865da8671253d758852fe
Jose Hernandez <josealfredo.066@gmail.com>
2025-07-01 12:36:01 -0400

    feat: enhance POST endpoint; create structured propertyData object for database submission

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -27,6 +27,36 @@ export const POST = async (request) => {
       .getAll('images')
       .filter((image) => image.name !== '');
 
+    // Create propertyData object for database
+    const propertyData = {
+      type: formData.get('type'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: {
+        street: formData.get('location.street'),
+        city: formData.get('location.city'),
+        state: formData.get('location.state'),
+        zipcode: formData.get('location.zipcode'),
+      },
+      beds: formData.get('beds'),
+      baths: formData.get('baths'),
+      square_feet: formData.get('square_feet'),
+      amenities,
+      rates: {
+        weekly: formData.get('rates.weekly'),
+        monthly: formData.get('rates.monthly'),
+        nightly: formData.get('rates.nightly'),
+      },
+      seller_info: {
+        name: formData.get('seller_info.name'),
+        email: formData.get('seller_info.email'),
+        phone: formData.get('seller_info.phone'),
+      },
+      images,
+    };
+
+    console.log(propertyData);
+
     return new Response(JSON.stringify({ message: 'Succes' }), {
       status: 200,
     });
