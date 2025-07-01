657812bac47c306a51d47e96968a3dea61fb72e8
Jose Hernandez <josealfredo.066@gmail.com>
2025-07-01 14:19:32 -0400

    feat: integrate Cloudinary for image uploads in property submission

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,6 +1,7 @@
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
 import { getSessionUser } from '@/utils/getSessionUser';
+import cloudinary from '@/config/cloudinary';
 
 // GET /api/properties
 export const GET = async (request) => {
@@ -64,8 +65,33 @@ export const POST = async (request) => {
         phone: formData.get('seller_info.phone'),
       },
       owner: userId,
-      // images,
     };
+
+    //Upload images to cloudinary
+    const imageUploadPromises = [];
+
+    for (const image of images) {
+      const imageBuffer = await image.arrayBuffer();
+      const imageArray = Array.from(new Uint8Array(imageBuffer));
+      const imageData = Buffer.from(imageArray);
+
+      //Convert the image data to base64
+      const imageBase64 = imageData.toString('base64');
+
+      // Make request to upload to cloudinary
+      const result = await cloudinary.uploader.upload(
+        `data:image/png;base64,${imageBase64}`,
+        { folder: 'DuoHome' }
+      );
+
+      imageUploadPromises.push(result.secure_url);
+
+      // Wait for all images to upload
+      const uploadedImages = await Promise.all(imageUploadPromises);
+
+      //Add uploaded images to the property data object
+      propertyData.images = uploadedImages;
+    }
 
     const newProperty = new Property(propertyData);
     await newProperty.save();
@@ -77,8 +103,7 @@ export const POST = async (request) => {
     // return new Response(JSON.stringify({ message: 'Succes' }), {
     //   status: 200,
     // });
-
   } catch (error) {
-    return new Response('failed to add property', { status: 500 });
+    return new Response(error, { status: 500 });
   }
 };
