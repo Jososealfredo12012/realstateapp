ce5482e453e7a258c24a8e1533f85af8f44cad4e
Jose Hernandez <josealfredo.066@gmail.com>
2025-07-02 11:49:31 -0400

    feat: implement DELETE endpoint for property management with ownership verification and user session check

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,5 +1,6 @@
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
+import { getSessionUser } from '@/utils/getSessionUser';
 
 // GET /api/properties/:id
 export const GET = async (request, props) => {
@@ -12,6 +13,43 @@ export const GET = async (request, props) => {
     if (!property) return new Response('Property not found', { status: 404 });
 
     return new Response(JSON.stringify(property), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
+
+// DELETE /api/properties/:id
+export const DELETE = async (request, props) => {
+  const params = await props.params;
+  try {
+    const propertyId = params.id;
+
+    const sessionUser = await getSessionUser();
+
+    //check for session
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response('User ID is required', { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    await connectDB();
+
+    const property = await Property.findById(propertyId);
+
+    if (!property) return new Response('Property not found', { status: 404 });
+
+    // Verify ownership
+    if (property.owner.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+
+    await property.deleteOne();
+
+    return new Response('property deleted', {
       status: 200,
     });
   } catch (error) {
