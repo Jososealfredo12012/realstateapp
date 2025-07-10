1c4cf25c8c88faf7265513587654768369024388
Jose Hernandez <josealfredo.066@gmail.com>
2025-07-09 21:34:44 -0400

    feat: implement message deletion functionality in Message component

--- app/api/messages/[id]/route.js
+++ app/api/messages/[id]/route.js
@@ -35,3 +35,31 @@ export const PUT = async (request, { params }) => {
     return new Response('Something went wrong', { status: 500 });
   }
 };
+
+
+// DELETE /api/messages/:id
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectDB();
+
+    const { id } = await params;
+
+    const message = await Message.findById(id);
+
+    if (!message) {
+      return new Response('Message Not Found', { status: 404 });
+    }
+
+    //Verify ownership
+    // if (message.recipient.toString() !== userId) {
+    //   return new Response('Unauthorized', { status: 401 });
+    // }
+
+    await message.deleteOne()
+
+    return new Response('Message Deleted', { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
--- components/Message.jsx
+++ components/Message.jsx
@@ -5,6 +5,7 @@ import { toast } from 'react-toastify';
 
 const Message = ({ message }) => {
   const [isRead, setIsRead] = useState(message.read);
+  const [isDeleted, setIsDeleted] = useState(false);
 
   const handleReadClick = async () => {
     try {
@@ -26,6 +27,26 @@ const Message = ({ message }) => {
       toast.error('Something went wrong');
     }
   };
+
+  const handleDeletedClick = async () => {
+    try {
+      const res = await fetch(`/api/messages/${message._id}`, {
+        method: 'DELETE',
+      });
+
+      if (res.status === 200) {
+        setIsDeleted(true);
+        toast.success('Message Deleted');
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong');
+    }
+  };
+
+  if (isDeleted) {
+    return null;
+  }
   return (
     <div className='relative bg-white p-4 rounded-md shadow-md border border-gray-200'>
       {!isRead && (
@@ -61,12 +82,14 @@ const Message = ({ message }) => {
       </ul>
       <button
         onClick={handleReadClick}
-        className={`mt-4 mr-3 ${
+        className={`mt-4 mr-3 hover:cursor-pointer ${
           isRead ? 'bg-gray-300' : 'bg-blue-500 text-white'
         }  py-1 px-3 rounded-md`}>
         {isRead ? 'Mark as new' : 'Mark as read'}
       </button>
-      <button className='mt-4 bg-red-500 text-white py-1 px-3 rounded-md'>
+      <button
+        onClick={handleDeletedClick}
+        className='mt-4 hover:cursor-pointer bg-red-500 text-white py-1 px-3 rounded-md'>
         Delete
       </button>
     </div>
