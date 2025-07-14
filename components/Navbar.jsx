764c985e0a91745dc2da8a4234ceab03183ab6ea
Jose Hernandez <josealfredo.066@gmail.com>
2025-07-14 13:12:34 -0400

    Cambia idioma de el navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -233,7 +233,7 @@ const Navbar = () => {
               className={`${
                 pathName === '/properties' && 'bg-gray-900'
               } text-white block rounded-md px-3 py-2 text-base font-medium`}>
-              Properties
+              Propiedades
             </Link>
             {isAdmin && (
               <Link
